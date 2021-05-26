94fb6843f7bd6144040fdc623573852b2c970301
Emirhan Bal <emir3434@outlook.com>
2021-05-26 20:32:42 +0300

    FRONTEND
    2 sayfa olarak düzenledim diğerlerini direkt sildim 2 page'de topladım... inş yanlış bişi yapmamışımdır

--- src/FrontEnd/LoginPage.java
+++ src/FrontEnd/LoginPage.java
@@ -1,77 +1,171 @@
-package FrontEnd;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
+import backend.User;
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.BevelBorder;
+public class LoginPage implements ActionListener {
+	
+	private static JLabel UserLabel;
+	private static JTextField UserText;
+	private static JLabel PasswordLabel;
+	private static JPasswordField PasswordText;
+	private static JButton LoginButton;
+	private static JButton RegisterButton;
+	private static JLabel Unsuccessfull;
+	private static JFrame frame = new JFrame("YEMEK ZEPETÝ");
+	private static JRadioButton ownerButton;
+	private static JRadioButton userButton;
+	private static JPanel panel;
+	private static int a=1;
+	public static void main(String args[]) {
+		
+		//LaunchPage launchPage = new LaunchPage ();
+		
+		
+		
+		panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBackground(Color.WHITE);
+		frame.setBackground(new Color(248, 248, 255));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,200);
+		frame.getContentPane().add(panel);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JList;
-import javax.swing.JScrollBar;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
-import javax.swing.AbstractListModel;
+		panel.setLayout(null);
+		
+		UserLabel = new JLabel("Username");
+		UserLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		UserLabel.setBounds(10,20,80,25);
+		panel.add(UserLabel);
+		
+		UserText = new JTextField();
+		UserText.setBounds(100,20,165,25);
+		panel.add(UserText);
+		
+		PasswordLabel = new JLabel("Password");
+		PasswordLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		PasswordLabel.setBounds(10,50,80,25);
+		panel.add(PasswordLabel);
+		
+		PasswordText = new JPasswordField();
+		PasswordText.setBounds(100,50,165,25);
+		panel.add(PasswordText);
+		
+		LoginButton = new JButton("Login");
+		LoginButton.setForeground(new Color(255, 255, 255));
+		LoginButton.setBackground(new Color(0, 0, 0));
+		LoginButton.setBounds(175,86,90,25);
+		LoginButton.addActionListener(new LoginPage());
+		panel.add(LoginButton);
+		
+		RegisterButton = new  JButton ("Register");
+		RegisterButton.setForeground(new Color(255, 255, 255));
+		RegisterButton.setBackground(new Color(0, 0, 0));
+		RegisterButton.setBounds(175,113,90,25);
+		RegisterButton.addActionListener(new LoginPage());
+		panel.add(RegisterButton);
+		frame.dispose();
+		
+		
+		
+		MainPage mainpage = new MainPage();
+		mainpage.setVisible(false);
+		
+		frame.setVisible(true);
+		
+	}
+	public boolean checkLogin() {
+		String username= UserText.getText();
+		String password = PasswordText.getText();
+		for(int i=0; i<User.getAllUsers().size(); i++) {
+			if(username.equals(User.getAllUsers().get(i).getUserName()) && password.equals(User.getAllUsers().get(i).getPassword())) {
+				return true;
+				
+			}
+		}
+		return false;
+	}
 
-public class LoginPage extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					LoginPage frame = new LoginPage();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==LoginButton) {
+			frame.dispose();
+			
+			boolean check = checkLogin();
+			if(check) {
+			MainPage mainpage = new MainPage();
+			mainpage.setVisible(true);
+				//anasayfaya yönlendir//
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Username or password is wrong!");
+				//KULLANICI ADI ÞÝFRE SIFIRLANACAK//
+				//!!!!BURADA DÝREKT WÝNDOWU KAPATIYOR BUNU ÇÖZMEMÝZ LAZIM!!!!//
+			}
+					
+		}
+		
+		else if(e.getSource()==RegisterButton) {
+			if(a==1) {
+				LoginButton.setVisible(false);
+				ownerButton = new JRadioButton("Owner");
+				ownerButton.setBounds(54, 82, 86, 14);
+				ownerButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(ownerButton.isSelected()) {
+							userButton.setSelected(false);
+						}
+					}
+				});
+				panel.add(ownerButton);
+				
+				
+				userButton = new JRadioButton("User");
+				userButton.setBounds(54, 99, 89, 17);
+				userButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(userButton.isSelected()) {
+							ownerButton.setSelected(false);
+						}
+					}
+				});
+				panel.add(userButton);
+				a++;
+			}
+			else {
+				String username= UserText.getText();
+				String password = PasswordText.getText();
+				for(int i=0; i<User.getAllUsers().size(); i++) {
+					if(username.equals(User.getAllUsers().get(i).getUserName())) {
+						JOptionPane.showMessageDialog(null, "username allready ALINMIÞ!");
+						UserText.setText("");
+						//USERTEXT SIFIRLANMASI LAZIM//
+					}
+				}
+				if(userButton.isSelected()){
+					System.out.println("USERBUTTON IS SELECTED");
+				}
+				else if(ownerButton.isSelected()) {
+					System.out.println("Owner is selected");
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "choose your type!");
 				}
 			}
-		});
+			
+		}
+		
+		
+		
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public LoginPage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel usernameLabel = new JLabel("Username:");
-		usernameLabel.setBounds(10, 11, 60, 14);
-		contentPane.add(usernameLabel);
-		
-		JList list = new JList();
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"image/name/price", "image/name/price", "image/name/price"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setBounds(10, 97, 414, 153);
-		contentPane.add(list);
-		
-		JLabel balanceLabel = new JLabel("Balance:");
-		balanceLabel.setBounds(10, 36, 60, 14);
-		contentPane.add(balanceLabel);
-		
-		JComboBox sepetim = new JComboBox();
-		sepetim.setBounds(359, 7, 30, 22);
-		contentPane.add(sepetim);
-		
-		JComboBox settingsBox = new JComboBox();
-		settingsBox.setBounds(399, 7, 30, 22);
-		contentPane.add(settingsBox);
-	}
 }
--- src/FrontEnd/MainPage.java
+++ src/FrontEnd/MainPage.java
@@ -1,19 +1,27 @@
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JScrollBar;
-import javax.swing.JButton;
-import javax.swing.JTextPane;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JScrollBar;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.AbstractListModel;
+import java.awt.Color;
+import javax.swing.JTable;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class MainPage {
-	
-	String usertype;
-	MainPage(String usertype){
-		this.usertype = usertype; }
+public class MainPage extends JFrame {
 
-	private JFrame frame;
+	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -22,8 +30,10 @@ public class MainPage {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainPage window = new MainPage();
-					window.frame.setVisible(true);
+					MainPage frame = new MainPage();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+					frame.setResizable(false);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -32,31 +42,182 @@ public class MainPage {
 	}
 
 	/**
-	 * Create the application.
+	 * Create the frame.
 	 */
 	public MainPage() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1000, 650);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 744, 541);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.setVisible(true);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(54, 124, 46, 14);
-		frame.getContentPane().add(lblNewLabel);
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 708, 84);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(154, 124, 46, 14);
-		frame.getContentPane().add(lblNewLabel_1);
+		JLabel unameLabel = new JLabel("Username:");
+		unameLabel.setBounds(118, 11, 67, 20);
+		panel.add(unameLabel);
+		
+		JLabel usernameLabel = new JLabel("Mert34");
+		usernameLabel.setBounds(248, 14, 46, 14);
+		panel.add(usernameLabel);
+		
+		JLabel utypeLabel = new JLabel("Usertype:");
+		utypeLabel.setBounds(118, 42, 67, 20);
+		panel.add(utypeLabel);
+		
+		JLabel usertypeLabel = new JLabel("Customer");
+		usertypeLabel.setBounds(248, 45, 57, 14);
+		panel.add(usertypeLabel);
+		
+		JLabel baLabel = new JLabel("Balance:");
+		baLabel.setBounds(534, 11, 67, 20);
+		panel.add(baLabel);
+		
+		JLabel balanceLabel = new JLabel("12.000");
+		balanceLabel.setBounds(636, 14, 46, 14);
+		panel.add(balanceLabel);
+		
+		JLabel locLabel = new JLabel("Location:");
+		locLabel.setBounds(534, 42, 67, 20);
+		panel.add(locLabel);
+		
+		JLabel locationLabel = new JLabel("Null");
+		locationLabel.setBounds(636, 48, 46, 14);
+		panel.add(locationLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.RED);
+		panel_1.setBounds(20, 106, 698, 385);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_5 = new JLabel("All Restaurants");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_5.setBounds(10, 11, 109, 40);
+		panel_1.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Name");
+		lblNewLabel_6.setBounds(313, 56, 57, 27);
+		panel_1.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("Location");
+		lblNewLabel_7.setBounds(478, 62, 46, 14);
+		panel_1.add(lblNewLabel_7);
+		
+		JButton btnNewButton = new JButton("Mc Donald's                                    \u0130stanbul");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(199, 87, 438, 38);
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Burger King");
+		btnNewButton_1.setBounds(199, 162, 438, 38);
+		panel_1.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Fatma Ev Yemekleri");
+		btnNewButton_2.setBounds(199, 238, 438, 38);
+		panel_1.add(btnNewButton_2);
+		
+		/*JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.BLUE);
+		panel_2.setBounds(10, 160, 718, 331);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton btnNewButton = new JButton("X");
+		btnNewButton.setBounds(629, 11, 52, 30);
+		panel_2.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("X");
+		btnNewButton_1.setBounds(629, 68, 52, 30);
+		panel_2.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("X");
+		btnNewButton_2.setBounds(629, 127, 52, 30);
+		panel_2.add(btnNewButton_2);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		
+		scrollBar.setBounds(691, 11, 17, 309);
+		panel_2.add(scrollBar);
+		
+		JButton btnNewButton_3 = new JButton("F\u0130YAT DE\u011E\u0130\u015ET\u0130RME BUTONU");
+		btnNewButton_3.setBounds(547, 15, 72, 23);
+		panel_2.add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("60.50");
+		btnNewButton_4.setBounds(547, 72, 72, 23);
+		panel_2.add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("New button");
+		btnNewButton_5.setBounds(547, 131, 72, 23);
+		panel_2.add(btnNewButton_5);
 		
 		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(290, 124, 46, 14);
-		frame.getContentPane().add(lblNewLabel_2);
+		lblNewLabel_2.setBounds(227, 11, 166, 30);
+		panel_2.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("New label");
+		lblNewLabel_2_1.setBounds(227, 65, 166, 30);
+		panel_2.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("New label");
+		lblNewLabel_2_2.setBounds(227, 127, 166, 30);
+		panel_2.add(lblNewLabel_2_2);
+		
+		JLabel lblNewLabel_2_3 = new JLabel("New label");
+		lblNewLabel_2_3.setBounds(227, 179, 166, 30);
+		panel_2.add(lblNewLabel_2_3);
+		
+		JLabel lblNewLabel_2_4 = new JLabel("New label");
+		lblNewLabel_2_4.setBounds(227, 236, 166, 30);
+		panel_2.add(lblNewLabel_2_4);
+		
+		JButton btnNewButton_5_1 = new JButton("New button");
+		btnNewButton_5_1.setBounds(547, 183, 72, 23);
+		panel_2.add(btnNewButton_5_1);
+		
+		JButton btnNewButton_5_2 = new JButton("New button");
+		btnNewButton_5_2.setBounds(547, 240, 72, 23);
+		panel_2.add(btnNewButton_5_2);
+		
+		JButton btnNewButton_2_1 = new JButton("X");
+		btnNewButton_2_1.setBounds(629, 183, 52, 30);
+		panel_2.add(btnNewButton_2_1);
+		
+		JButton btnNewButton_2_2 = new JButton("X");
+		btnNewButton_2_2.setBounds(629, 240, 52, 30);
+		panel_2.add(btnNewButton_2_2);
+		
+		JButton btnNewButton_6 = new JButton("ADD PRODUCT");
+		btnNewButton_6.setBounds(227, 290, 123, 30);
+		panel_2.add(btnNewButton_6);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 106, 708, 45);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Burger King");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(299, 11, 90, 23);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("IMAGE");
+		lblNewLabel_1.setBounds(22, 11, 78, 20);
+		panel_1.add(lblNewLabel_1);
+		panel_2.setVisible(true);
+		
+		scrollBar.addAdjustmentListener(new AdjustmentListener() {
+			public void adjustmentValueChanged(AdjustmentEvent e) {
+			}
+		});*/
 	}
 }
